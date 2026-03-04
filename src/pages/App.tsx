8bd37b2201856ed1f4963e3b5c84c167de501556
Juan Antonio <juandevnjv@gmail.com>
2026-03-04 13:15:50 +0100

    feat: agregar componente Hero y aplicar estilos de texto en App

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,14 +1,17 @@
 import type { FC } from "react";
+import styles from "@/assets/css/index.module.css";
+import Hero from "@/components/Hero";
 
 const App: FC = () => {
   return (
-    <section className={` flex flex-col items-center justify-center gap-4 text-center`}>
-      <h1 className="text-4xl md:text-7xl font-bold p-4">Inicio</h1>
-      <p className="max-w-2xl text-2xl  md:text-3xl p-4">
+    <Hero>
+      <p
+        className={`text-3xl md:text-5xl md:max-w-2xl font-black text-green-300 px-4 ${styles.upperCase}`}
+      >
         Bienvenido a Horas Adicionales. Aquí puedes registrar tus jornadas, organizar tus perfiles
-        de trabajo y consultar tu información de formaº rápida.
+        de trabajo y consultar tu información de forma rápida.
       </p>
-    </section>
+    </Hero>
   );
 };
 
