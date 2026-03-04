9d53f24207830e36da43aec5fd5576470cdc473a
Juan Antonio <juandevnjv@gmail.com>
2026-03-04 13:34:13 +0100

    feat: actualizar estilos de overlay y agregar sombra al encabezado principal

--- src/pages/layouts/MainHeader.tsx
+++ src/pages/layouts/MainHeader.tsx
@@ -3,11 +3,14 @@ import CurrentUser from "@/components/CurrentUser";
 import DesktopViewOnly from "@/components/DesktopViewOnly";
 import Menu from "@/components/Menu";
 import MenuBars from "@/components/MenuBars";
+import styles from "@/assets/css/index.module.css";
 
 const MainHeader = () => {
   return (
-    <header className="sticky top-0 bg-overlay-50 flex gap-4 p-4 justify-between items-center">
-      <DesktopViewOnly tag="section">
+    <header
+      className={`sticky top-0 bg-overlay-90 flex gap-4 p-4 justify-between items-center ${styles.borderBottomShadow}`}
+    >
+      <DesktopViewOnly tag="section" className="-ml-4">
         <Brand />
       </DesktopViewOnly>
       <Menu />
